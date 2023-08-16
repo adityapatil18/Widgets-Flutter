53ae446e6d391b94cec11215e67d306be269ced4
adityapatil18 <adityagangane2018@gmail.com>
2023-08-16 18:40:31 +0530

    chnages done

--- lib/main.dart
+++ lib/main.dart
@@ -16,25 +16,26 @@ import 'package:widgets/Home/Widgets/animated_rotation.dart';
 import 'package:widgets/Home/Widgets/animated_size.dart';
 import 'package:widgets/Home/Widgets/animated_switcher.dart';
 import 'package:widgets/Home/Widgets/app_bar.dart';
-import 'package:widgets/Home/aspect_ratio.dart';
-import 'package:widgets/Home/auto_complete.dart';
-import 'package:widgets/Home/back_drof_filter.dart';
-import 'package:widgets/Home/banner.dart';
-import 'package:widgets/Home/baseline.dart';
-import 'package:widgets/Home/block_semantics.dart';
-import 'package:widgets/Home/bottom_navigation_bar.dart';
-import 'package:widgets/Home/bottom_sheet.dart';
-import 'package:widgets/Home/builder.dart';
-import 'package:widgets/Home/card.dart';
-import 'package:widgets/Home/center.dart';
-import 'package:widgets/Home/home_page.dart';
-import 'package:widgets/check_box.dart';
-import 'package:widgets/checkbox_List_Tile.dart';
-import 'package:widgets/chip.dart';
-import 'package:widgets/choice_chip.dart';
+import 'package:widgets/Home/Widgets/aspect_ratio.dart';
+
 
 import 'Home/Widgets/animated_default_text_style.dart';
 import 'Home/Widgets/animated_modal_barrier.dart';
+import 'Home/Widgets/auto_complete.dart';
+import 'Home/Widgets/back_drof_filter.dart';
+import 'Home/Widgets/banner.dart';
+import 'Home/Widgets/baseline.dart';
+import 'Home/Widgets/block_semantics.dart';
+import 'Home/Widgets/bottom_navigation_bar.dart';
+import 'Home/Widgets/bottom_sheet.dart';
+import 'Home/Widgets/builder.dart';
+import 'Home/Widgets/card.dart';
+import 'Home/Widgets/center.dart';
+import 'Home/Widgets/check_box.dart';
+import 'Home/Widgets/checkbox_List_Tile.dart';
+import 'Home/Widgets/chip.dart';
+import 'Home/Widgets/choice_chip.dart';
+import 'Home/home_page.dart';
 
 void main() {
   runApp(Widgets());
